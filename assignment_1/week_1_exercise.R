7d15fcf3b48edad8a2c1410c2713084f2646e0a9
Marcio Woitek <woitek@usp.br>
2021-01-04 19:15:06 -0300

    Completed 1st assignment

--- assignment_1/week_1_exercise.R
+++ assignment_1/week_1_exercise.R
@@ -18,8 +18,8 @@
 
 # %% name="setup" tags=["remove_cell"]
 knitr::opts_chunk$set(echo = TRUE)
-library(tidyverse)
-cel <- drop_na(read_csv("cel_volden_wiseman_coursera.csv"))
+suppressPackageStartupMessages(library(tidyverse))
+cel <- suppressMessages(drop_na(read_csv("cel_volden_wiseman_coursera.csv")))
 
 # %% [markdown]
 # Your objective is to replicate these figures, created using the Center of
@@ -44,6 +44,20 @@ cel <- drop_na(read_csv("cel_volden_wiseman_coursera.csv"))
 # * Make sure the axis labels are correct.
 
 # %% name="exercise1" tags=["remove_input"]
+# Get only the data corresponding to the 115th Congress.
+# Get only the columns that matter for this exercise.
+data_plot_1 <- cel %>%
+    filter(congress == 115) %>%
+    select(dwnom1, all_pass, female)
+
+# Recode the `female` variable, and rename it as `Gender`:
+data_plot_1$female <- recode(data_plot_1$female, `0` = "Male", `1` = "Female")
+data_plot_1 <- rename(data_plot_1, Gender = female)
+
+# Plot the figure as specified above:
+ggplot(data_plot_1, aes(x = dwnom1, y = all_pass, color = Gender)) +
+    geom_point() +
+    labs(x = "Ideology", y = "Bills Passed")
 
 # %% [markdown]
 # ## Exercise 2
@@ -55,8 +69,8 @@ cel <- drop_na(read_csv("cel_volden_wiseman_coursera.csv"))
 # * Make sure you recode the data for the `female` variable to generate the
 # correct labels for the legend. Rename that column `Gender` (you may have
 # already done this in the last exercise).
-# * Make sure you recode the data for `majority` variable to generate the correct
-# labels of the faceted figures.
+# * Make sure you recode the data for `majority` variable to generate the
+# correct labels of the faceted figures.
 # * Set the color aesthetic in the `ggplot()` function to make the color of the
 # dots change based on `Gender`.
 # * Use `scale_color_manual()` to set the colors to green for males and orange
@@ -64,6 +78,27 @@ cel <- drop_na(read_csv("cel_volden_wiseman_coursera.csv"))
 # * Make sure the axis labels are correct.
 
 # %% name="exercise2" tags=["remove_input"]
+# Get only the data corresponding to the 115th Congress.
+# Get only the columns that matter for this exercise.
+data_plot_2 <- cel %>%
+    filter(congress == 115) %>%
+    select(votepct, all_pass, female, majority)
+
+# Recode the `female` variable, and rename it as `Gender`:
+data_plot_2$female <- recode(data_plot_2$female, `0` = "Male", `1` = "Female")
+data_plot_2 <- rename(data_plot_2, Gender = female)
+
+# Recode the `majority` variable:
+data_plot_2$majority <- recode(
+    data_plot_2$majority, `0` = "Minority", `1` = "Majority"
+)
+
+# Plot the figure as specified above:
+ggplot(data_plot_2, aes(x = votepct, y = all_pass, color = Gender)) +
+    geom_point() +
+    labs(x = "Vote Percentage", y = "Bills Passed") +
+    scale_color_manual(values = c("orange", "green")) +
+    facet_wrap(vars(majority))
 
 # %% [markdown]
 # ## Exercise 3
@@ -76,3 +111,22 @@ cel <- drop_na(read_csv("cel_volden_wiseman_coursera.csv"))
 # * Make sure the axis labels and figure title are correct.
 
 # %% name="exercise3" tags=["remove_input"]
+# Get only the data corresponding to the 115th Congress.
+# Get only the columns that matter for this exercise.
+data_plot_3 <- cel %>%
+    filter(congress == 115) %>%
+    select(les, majority)
+
+# Recode the `majority` variable:
+data_plot_3$majority <- recode(
+    data_plot_3$majority, `0` = "Minority", `1` = "Majority"
+)
+
+# Plot the figure as specified above:
+ggplot(data_plot_3, aes(x = majority, y = les)) +
+    geom_boxplot() +
+    labs(
+        x = "Majority or Minority",
+        y = "Legislative Effectiveness",
+        title = "LES in the 115th Congress"
+    )
